428ce7beafa6a2a1756b6cad2a798a72eb8d6e54
bhryan perpetuo <bhryan.perpetuo@gmail.com>
2020-01-16 12:27:05 -0300

    backend added Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,7 +10,8 @@ mongoose.connect('mongodb+srv://bhryan:perpetuo96@cluster0-6fa7o.mongodb.net/fin
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(33333);
