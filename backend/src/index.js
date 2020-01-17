359b4b7f5b7e8cd77d75f62186712658c44f3ee1
bhryan perpetuo <bhryan.perpetuo@gmail.com>
2020-01-17 12:21:33 -0300

    backend included websockets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
+const http = require('http');
+const { setupWebsocket } = require('./websocket');
 const cors = require('cors');
 
 const app = express();
+const server = http.Server(app);
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://bhryan:perpetuo96@cluster0-6fa7o.mongodb.net/finddev?retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -14,4 +18,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(33334);
+server.listen(33334);
